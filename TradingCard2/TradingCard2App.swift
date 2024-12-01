db1e75462ded564844c498ee320b97ec31eb87fa
Elaine Fang <efang@lcs.on.ca>
2024-11-30 21:30:42 -0500

    Initial Commit

---
+++ TradingCard2/TradingCard2App.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCard2App.swift
+//  TradingCard2
+//
+//  Created by Fang Elaine on 2024/11/30.
+//
+
+import SwiftUI
+
+@main
+struct TradingCard2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
